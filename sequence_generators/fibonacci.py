4e972cb9cf979dc0cd5ad8300a7baf60735b3fe9
Hector Munoz <hector.munoz@rackspace.com>
2014-06-16 12:39:03 -0500

    rename variable with reserved name

--- sequence_generators/fibonacci.py
+++ sequence_generators/fibonacci.py
@@ -1,7 +1,7 @@
 def generate_sequence(size):
-    list = [0, 1]
+    sequence = [0, 1]
     if size < 3:
-        return list[0: size]
+        return sequence[0: size]
     for index in range(2, size):
-        list.append(list[index - 2] + list[index - 1])
-    return list
+        sequence.append(sequence[index - 2] + sequence[index - 1])
+    return sequence
